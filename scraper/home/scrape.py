c7351a43b828e0e5ef8ade654f16a28978b32e1e
ittsfatiixx <43836400+ittsfatiixx@users.noreply.github.com>
2023-02-17 21:03:04 +0530

    Update scrape.py

--- scraper/home/scrape.py
+++ scraper/home/scrape.py
@@ -5,19 +5,22 @@ from bs4 import BeautifulSoup
 # mydict={'name':[],'rating':[],'price':[]}
 from pymongo import MongoClient
 
+'''connect with MongoDb'''
 client=MongoClient('localhost',27017)
 db_name = client["mydb"]
 collection=db_name['data']
 
 
 
-
+'''scrape'''
 response= requests.get('https://www.flipkart.com/search?q=mobile+phones&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&as-pos=1&as-type=HISTORY')
 soup=BeautifulSoup(response.content,'html.parser')
 phones=soup.find_all('div',class_="_3pLy-c row")
-names=soup.find_all('div',class_="_4rR01T")
 
+# names=soup.find_all('div',class_="_4rR01T")
 # prices=soup.find_all('div',class_="_30jeq3 _1_WHN1")
+
+'''iterate through each phone data and insert it in mongo collection one by one'''
 for phone in phones:
     mydict={}
     name=phone.find('div',class_='_4rR01T').text
